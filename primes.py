7f5b558f2fe21702d36bfd1abf8851aa2a3c743d
Katia Bennamane <95385281+Katbenn@users.noreply.github.com>
2022-12-09 15:25:10 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,20 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"Number of primes {number_of_primes}: should have been greater than 0.")
 
-if number_of_primes <= 0:
-    raise ValueError(f"Number of primes {number_of_primes} must greater than 0.")
-
-list = []
-index = 2
-while len(list) < number_of_primes:
-    if len(list) == 0:
-        list.append(index)
-    else:
-        for p in list:
-            if index % p == 0:
-                break
-        else:
+    list = []
+    index = 2
+    while len(list) < number_of_primes:
+        if len(list) == 0:
             list.append(index)
-    index += 1
-return list
+        else:
+            for p in list:
+                if index % p == 0:
+                    break
+            else:
+                list.append(index)
+        index += 1
+    return list
